9a440fb48c0fbbea5fe5d3a728526bc84a44456f
Roy <157291828+Rigriz@users.noreply.github.com>
2026-02-08 12:32:57 +0530

    Update list.js

--- pages/api/list.js
+++ pages/api/list.js
@@ -2,9 +2,10 @@ import { supabase } from '../../lib/supabase'
 
 export default async function handler(req, res) {
 
-  const { data } = await supabase
+  const { data, error } = await supabase
     .from('mi')
     .select('*')
+    .order('last_seen', { ascending: false })
 
   res.json(data || [])
 }
